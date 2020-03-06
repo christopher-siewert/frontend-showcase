516e5fccd0ea2544c885aaf99c0da57d05cc0b24
Christopher Siewert <christopher.siewert@live.ca>
2020-03-06 14:25:05 -0800

    Removed worst sites

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Frontend Showcase
 
-In 2016 I wanted to learn web developement so I went on a coding spree and made many front end web projects. This is a little journey through that process.
+This is a showcase of a few front end web projects I made when learning to code.
 
 Visit [christopher-siewert.github.io/frontend-showcase](https://christopher-siewert.github.io/frontend-showcase) to see it.
--- game-of-life/js/index.js
+++ game-of-life/js/index.js
@@ -1,118 +1,107 @@
 var events = {};
 var running = true;
 
-var Parent = React.createClass({ displayName: "Parent",
-  getInitialState: function () {
+var Parent = React.createClass({
+  displayName: "Parent",
+  getInitialState: function() {
     // Build cell array
     var cells = [];
     for (var y = 0; y < 50; y++) {
       for (var x = 0; x < 50; x++) {
-        cells.push(React.createElement(Cell, { id: y + "-" + x, cells: cells }));
+        cells.push(React.createElement(Cell, {id: y + "-" + x, cells: cells}));
       }
     }
     return {
       cells: cells,
-      count: 0 };
-
-
+      count: 0
+    };
   },
-  componentDidMount: function () {
+  componentDidMount: function() {
     $(events).on("count", this.count);
-
   },
-  count: function () {
+  count: function() {
     var add = this.state.count + 1;
     this.setState({
-      count: add });
-
-
+      count: add
+    });
   },
-  onPlay: function () {
+  onPlay: function() {
     if (running) {
       clearInterval(interval);
       running = !running;
     } else if (!running) {
-      interval = setInterval(function () {
+      interval = setInterval(function() {
         $(events).trigger("count");
         $(events).trigger("calculate");
         $(events).trigger("renderNext");
       }, 100);
       running = !running;
     }
-
   },
-  onClear: function () {
+  onClear: function() {
     $(events).trigger("clear");
     this.setState({
-      count: 0 });
-
+      count: 0
+    });
   },
-  render: function () {
-    return (
-      React.createElement("div", null,
-      React.createElement("div", { className: "box" },
-      this.state.cells),
+  render: function() {
+    return React.createElement(
+      "div",
+      null,
+      React.createElement("div", {className: "box"}, this.state.cells),
 
-      React.createElement("div", { className: "footer" },
-      React.createElement("button", { onClick: this.onPlay }, "Play/Pause"),
-      React.createElement("button", { onClick: this.onClear }, "Clear"),
-      React.createElement("div", null, "Generation: ", this.state.count))));
+      React.createElement(
+        "div",
+        {className: "footer"},
+        React.createElement("button", {onClick: this.onPlay}, "Play/Pause"),
+        React.createElement("button", {onClick: this.onClear}, "Clear"),
+        React.createElement("div", null, "Generation: ", this.state.count)
+      )
+    );
+  }
+});
 
-
-
-  } });
-
-
-var Cell = React.createClass({ displayName: "Cell",
-  getInitialState: function () {
+var Cell = React.createClass({
+  displayName: "Cell",
+  getInitialState: function() {
     return {
       alive: Math.random() < 0.7 ? false : true,
-      nextState: false };
-
-
+      nextState: false
+    };
   },
-  componentDidMount: function () {
-    this.props.cells[this.props.id] = this; // place cell as object in global array 
+  componentDidMount: function() {
+    this.props.cells[this.props.id] = this; // place cell as object in global array
     $(events).on("calculate", this.calculate);
     $(events).on("renderNext", this.renderNext);
     $(events).on("clear", this.clear);
-
   },
-  onClick: function (e) {
+  onClick: function(e) {
     this.setState({
-      alive: !this.state.alive });
-
-
+      alive: !this.state.alive
+    });
   },
-  clear: function () {
+  clear: function() {
     this.setState({
-      alive: false });
-
+      alive: false
+    });
   },
-  calculate: function () {
+  calculate: function() {
     var id = this.props.id.split("-");
     var y = +id[0];
     var x = +id[1];
     this.state.nextState = false;
-    debugger;
     if (this.checkCell(y, x) == 3) {
-      debugger;
       this.state.nextState = true;
     } else if (this.checkCell(y, x) == 4) {
-      debugger;
       this.state.nextState = this.state.alive;
     } else {
-      debugger;
       this.state.nextState = false;
     }
-
   },
-  isAlive: function (y, x) {
+  isAlive: function(y, x) {
     return this.props.cells[y + "-" + x].state.alive;
-
   },
-  checkCell: function (y, x) {
-    debugger;
+  checkCell: function(y, x) {
     var neighbors = [];
     for (var i = -1; i < 2; i++) {
       for (var j = -1; j < 2; j++) {
@@ -128,34 +117,34 @@ var Cell = React.createClass({ displayName: "Cell",
         } else if (bumpY > 49) {
           bumpY = 0;
         }
-        debugger;
         neighbors.push(this.isAlive(bumpY, bumpX));
       }
     }
-    debugger;
-    neighbors = neighbors.filter(function (state) {
+    neighbors = neighbors.filter(function(state) {
       return state == true;
     });
-    debugger;
     return neighbors.length;
-
   },
-  renderNext: function () {
+  renderNext: function() {
     this.setState({
-      alive: this.state.nextState });
-
-
+      alive: this.state.nextState
+    });
   },
-  render: function () {
-    return (
-      React.createElement("div", { id: this.props.id, onClick: this.onClick, className: this.state.alive ? "alive" : "dead" }));
+  render: function() {
+    return React.createElement("div", {
+      id: this.props.id,
+      onClick: this.onClick,
+      className: this.state.alive ? "alive" : "dead"
+    });
+  }
+});
 
-  } });
+ReactDOM.render(
+  React.createElement(Parent, null),
+  document.getElementById("app")
+);
 
-
-ReactDOM.render(React.createElement(Parent, null), document.getElementById('app'));
-
-var interval = setInterval(function () {
+var interval = setInterval(function() {
   $(events).trigger("count");
   $(events).trigger("calculate");
   $(events).trigger("renderNext");
